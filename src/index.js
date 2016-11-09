0e097162081b701d80fa727007e2b3652c9cf546
dns2316 <dns2316@gmail.com>
2016-11-09 14:03:38 +0200

    skb2fullname_=1_test8

--- src/index.js
+++ src/index.js
@@ -5,16 +5,20 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
+  console.log(req.query.fullname);
+  console.log(req.query.fullname.length);
   const fullname = req.query.fullname.split(' ');
   console.log(fullname.length);
+
   const result = `${fullname[fullname.length -1]} ${fullname[0].charAt(0)}.`;
+
   if (fullname.length === 3) {
     res.send(`${result} ${fullname[fullname.length-2].charAt(0)}.`);
   }
   if (fullname.length === 2) {
     res.send(result);
   }
-  if (fullname.length === 1) {
+  if (fullname.length === 1 && fullname[0].charAt(0 > 1)) {
     res.send(fullname[0]);
   }
   else {
