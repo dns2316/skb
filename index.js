f3f2a88a62a71455f63e2a76ffee1d552b3990c5
dns2316 <dns2316@gmail.com>
2016-11-11 15:01:48 +0200

    skb2c=1_test=8

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ import pug from 'pug';
 const app = express();
 const path = require('path');
 const bug = function (variable) {
-  return '\nTipster told: '.console.log(variable);
+  return '\nTipster told: ' + console.log(variable);
 };
 let responceToHtml = 'hello world!';
 const logger = require('morgan');
 
 app.use(cors());
 app.use(express.static(path.join(__dirname + '.../view')));
-app.set('view engine', 'jade');
 app.use(morgan('dev'));
-// const re = new RegExp('(fullname=)?(\s)(a-zA-Z)?'); - this code != 1
 
 // ******* code from https://github.com/iCoderXXI/skill-branch/commit/ff1cac53939622f9114d4d5d3d50953f0976c486
 const f2u = function(s) {
@@ -42,12 +40,19 @@ app.get('/2b', (req, res) => {
 // ******* end code from https://github.com/iCoderXXI/skill-branch/commit/ff1cac53939622f9114d4d5d3d50953f0976c486
 
 app.get('/2c', (req, res) => {
-  const re2c = new RegExp('[^\/]*\/');
+  const re2c = new RegExp('^(.*username=)?@?(\w+:)?(\/\/)?(.+\.[^\/]*\/)?([^\/\?]+)(\/)?(\/?)?.*$');
   const url = req.originalUrl;
-  console.log(url);
+
+  // ******* method remove non-desired
+  // const newMethod = url.replace(/^username=/, ''.replace(/\*/, ''));
+  // bug(newMethod);
+  // ******* end method remove non-desired
+
   // res.send('We got your request!');
   // res.sendFile(path.join(__dirname + '/view/responce.html'));
-  res.render('responce.jade');
+  const resNick = url.match(re2c)[5];
+  bug(resNick);
+  res.send ('@' + resNick);
 });
 app.listen(80, () => {
   console.log('App working on port 80!');
